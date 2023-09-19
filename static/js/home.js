5ea332aa318b80ec07db148fabe80e2428942126
hiihaily <hiihaily@gmail.com>
2023-09-18 22:57:39 -0700

    Updated to respond with the updated tictactoe layout in home.html

--- static/js/home.js
+++ static/js/home.js
@@ -27,79 +27,86 @@ window.onload = function () {
   });
 };
 
-const cells = document.querySelectorAll('[data-cell]');
-const status = document.getElementById('status');
-const resetButton = document.getElementById('reset-button');
+document.addEventListener("DOMContentLoaded", () => {
+  const cells = document.querySelectorAll(".cell");
+  const resetButton = document.querySelector('.reset-button');
+  let currentPlayer = 'X';
 
-let currentPlayer = 'X';
-let board = ['', '', '', '', '', '', '', '', ''];
-let gameActive = true;
-
-function checkWinner() {
-  const winPatterns = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8],
-      [0, 3, 6], [1, 4, 7], [2, 5, 8],
-      [0, 4, 8], [2, 4, 6]
-  ];
-
-  for (const pattern of winPatterns) {
-      const [a, b, c] = pattern;
-      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-          // Player wins
-          updateStatistics("win");
-          return board[a];
-      }
-  }
-
-  if (board.includes('')) {
-      return null; // Game still ongoing
-  } else {
-      // It's a tie
-      updateStatistics("tie");
-      return 'T';
-  }
-}
-
-
-function handleCellClick(e) {
-    const cell = e.target;
-    const cellIndex = cell.getAttribute('data-cell');
-
-    if (board[cellIndex] === '' && gameActive) {
-        board[cellIndex] = currentPlayer;
-        cell.textContent = currentPlayer;
-        cell.classList.add(currentPlayer);
-        
-        const winner = checkWinner();
-        if (winner) {
-            if (winner === 'T') {
-                status.textContent = "It's a tie!";
-            } else {
-                status.textContent = `Player ${winner} wins!`;
-            }
-            gameActive = false;
-        } else {
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-            status.textContent = `Player ${currentPlayer}'s turn`;
-        }
-    }
-}
-
-function handleResetClick() {
+  //Game reset-function
+  function handleResetClick() {
     currentPlayer = 'X';
-    board = ['', '', '', '', '', '', '', '', ''];
-    gameActive = true;
-    status.textContent = "Player X's turn";
     cells.forEach(cell => {
-        cell.textContent = '';
-        cell.classList.remove('X', 'O');
+      cell.textContent = '';
+      cell.classList.remove('X', 'O');
     });
-}
+  }
 
-cells.forEach(cell => cell.addEventListener('click', handleCellClick));
-resetButton.addEventListener('click', handleResetClick);
+  resetButton.addEventListener('click', handleResetClick);
 
-handleResetClick(); // Initialize the game
+  cells.forEach((cell) => {
+    cell.addEventListener("click", (e) => {
+      // If the clicked cell is empty:
+      if (e.target.textContent == "") {
+        // Place the current player's mark (X or O) in the cell.
+        e.target.textContent = currentPlayer;
+
+        // Check if the current move resulted in a win.
+        if (checkWinner()) {
+          setTimeout(() => {
+            alert(currentPlayer + " won this round!");
+            location.reload(); // Reload the game.
+          }, 100);
+        }
+        // If not a win, check if the game is a tie.
+        else if (isTie()) {
+          setTimeout(() => {
+            alert("It's a tie!");
+            location.reload(); // Reload the game.
+          }, 100); // schedules a function to run after a delay ensuring that the last move is visually rendered on the screen before the alert pops up
+        }
+        // If not a win and not a tie, switch to the other player.
+        else {
+          currentPlayer = currentPlayer === "X" ? "O" : "X";
+        }
+      }
+    });
+  });
+
+  // Function to check if there's a winning combination on the board.
+  function checkWinner() {
+    // List of all potential winning combinations in tic-tac-toe.
+    const winCombination = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    // Loop through each potential winning combination.
+    for (const combination of winCombination) {
+      // If all cells in a combination have the same non-empty value, return true (indicating a win).
+      if (
+        cells[combination[0]].textContent !== "" && //Checks if the first cell is not empty
+        cells[combination[0]].textContent ===
+          cells[combination[1]].textContent && // If the content of the first cell is the same as the content of the second cell in the current combination.
+        cells[combination[1]].textContent === cells[combination[2]].textContent // checks if the content of the second cell is the same as the content of the third cell in our combination.
+      ) {
+        return true;
+      }
+    }
+    return false; // If no winning combination found, return false.
+  }
+
+  // Function to check if all cells are filled and the game is a tie.
+  function isTie() {
+    // Check if every cell is filled (none are empty).
+    return [...cells].every((cell) => cell.textContent !== "");
+  }
+});
 
 
 let gamesPlayed = 0;
