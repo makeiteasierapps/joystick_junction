980f54e59ebb65d111ccf699efd6f7b748fc2901
Shaun Offenbacher <130625311+makeiteasierapps@users.noreply.github.com>
2023-09-25 23:38:39 -0600

    updated imports

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 import AuthService from './auth.js';
 import homePage from '../home.html';
-import { db } from './firebaseConfig.js';
+import { db } from '../firebaseConfig.js';
 import { doc, updateDoc } from 'firebase/firestore';
 
 // This event listener is triggered when the 'create-account' button is clicked.
