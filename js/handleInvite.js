43c76c19985d20adbd5524b66fd22bb3b0e0cedf
Shaun Offenbacher <makeiteasierapps@gmail.com>
2023-09-28 16:06:04 -0600

    set up backend game play

--- js/handleInvite.js
+++ js/handleInvite.js
@@ -1,4 +1,12 @@
-import { collection, query, onSnapshot, addDoc } from 'firebase/firestore';
+import {
+    collection,
+    query,
+    onSnapshot,
+    addDoc,
+    doc,
+    deleteDoc,
+    setDoc,
+} from 'firebase/firestore';
 import { db, auth } from '../firebaseConfig.js';
 
 export function listenForInvites(userId) {
@@ -11,10 +19,85 @@ export function listenForInvites(userId) {
     onSnapshot(q, (querySnapshot) => {
         querySnapshot.docChanges().forEach((change) => {
             if (change.type === 'added') {
-                console.log('New invite: ', change.doc.data());
+                const inviteData = change.doc.data();
+                const inviteFrom = inviteData.from;
+                const inviteId = change.doc.id;
+                const accepted = confirm(
+                    `New invite from ${inviteData.from}. Do you accept?`
+                );
+                handleInviteResponse(inviteFrom, inviteId, accepted);
             }
         });
     });
+}
+
+function handleInviteResponse(inviteFrom, inviteId, accepted) {
+    if (accepted) {
+        // Create a new document in the 'games' collection of both users
+        const gameData = {
+            players: [auth.currentUser.uid, inviteFrom],
+            timestamp: Date.now(),
+            // Add any other game data you need here
+        };
+
+        const gameId = `${auth.currentUser.uid}-${inviteFrom}`;
+
+        // creates a new document in the 'games' collection of the user accepting the invite
+        const currentUserGameDoc = doc(
+            db,
+            'users',
+            auth.currentUser.uid,
+            'games',
+            gameId
+        );
+        setDoc(currentUserGameDoc, gameData);
+
+        // creates a new document in the 'games' collection of the user who sent the invite
+        const inviteFromGameDoc = doc(db, 'users', inviteFrom, 'games', gameId);
+        setDoc(inviteFromGameDoc, gameData);
+
+        // Create a new document for the game board
+        const gameBoardData = {
+            cells: Array(9).fill(0), // Initialize an array of 9 0s to represent an empty game board
+            // Add any other game board data you need here
+        };
+
+        // Create a new document in the 'gameBoard' subcollection of the 'games' collection for the user accepting the invite
+        const currentUserGameBoardDoc = doc(
+            db,
+            'users',
+            auth.currentUser.uid,
+            'games',
+            gameId,
+            'gameBoards',
+            'gameBoard'
+        );
+        setDoc(currentUserGameBoardDoc, gameBoardData);
+
+        // Create a new document in the 'gameBoard' subcollection of the 'games' collection for the user who sent the invite
+        const inviteFromGameBoardDoc = doc(
+            db,
+            'users',
+            inviteFrom,
+            'games',
+            gameId,
+            'gameBoards',
+            'gameBoard'
+        );
+        setDoc(inviteFromGameBoardDoc, gameBoardData);
+        // set the gameId in sessionStorage so we can access it later
+        sessionStorage.setItem('gameId', gameId);
+    }
+
+    // Remove the invite from the 'invites' collection
+    const inviteDoc = doc(
+        db,
+        'users',
+        auth.currentUser.uid,
+        'invites',
+        inviteId
+    );
+    deleteDoc(inviteDoc);
 }
 
 export function sendInvite(uid) {
